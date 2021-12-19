c6758c487aba7f12badbc745f3943598de87c60f
Achintya241991 <47078654+Achintya241991@users.noreply.github.com>
2021-12-19 18:25:56 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowllingBallTavisca
+{
+    #region Comment
+    /* 
+      1. As stated following points in the requirement
+       > Does not need to check for valid rolls.
+       > Does not need to check for correct number of rolls and frames.
+       > Does not need to not provide scores for intermediate frames.
+       > The solution should assume valid inputs at all times and does not need to include unnecessary validations
+         trying to prevent bad input data.
+       2. Code can be segregated into different levels, client (Main), class (BL) and interface (Service), but 
+    */
+    #endregion
+
+    #region Service
+    public interface IBallingScore
+    {
+        int GetTotalScore(List<int> throwBalls);
+    }
+    #endregion
+
+    #region BusinessLogic
+    public class BowlingBallAlgo : IBallingScore
+    {
+        int currentRoll = 0;
+        internal List<int> Throws { get; private set; }
+
+        public int GetTotalScore(List<int> throwBall)
+        {
+            int frames = 0;
+            List<int> currentTotal = new List<int>();
+            Throws = throwBall;
+            while (frames != 10)
+            {
+                var newFrame = CreateFrame(Throws);
+                if (IsStrike(newFrame))
+                {
+                    currentTotal.Add(newFrame.Sum() + GetNextTwoRollPoints(currentRoll));
+                }
+                else if (IsSpare(newFrame))
+                {
+                    currentTotal.Add(newFrame.Sum() + GetNextOneRollPoints(currentRoll));
+                }
+                else
+                {
+                    currentTotal.Add(newFrame.Sum());
+                }
+                frames++;
+            }
+            return currentTotal.Sum();
+        }
+
+        private int GetNextTwoRollPoints(int currentRoll)
+        {
+            return Throws[currentRoll] + Throws[currentRoll + 1];
+        }
+
+        private int GetNextOneRollPoints(int currentRoll)
+        {
+            return Throws[currentRoll];
+        }
+
+        private bool IsSpare(List<int> throws)
+        {
+            return throws.Take(2).Sum() == 10;
+        }
+
+        private bool IsStrike(List<int> throws)
+        {
+            return throws.First() == 10;
+        }
+
+        private List<int> CreateFrame(List<int> throws)
+        {
+            List<int> newframe = new List<int>();
+            if (throws[currentRoll] == 10) //Single value frame for strike
+            {
+                newframe.Add(throws[currentRoll]);
+                currentRoll++;
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++) //Double value frame
+                {
+                    newframe.Add(throws[currentRoll]);
+                    currentRoll++;
+                }
+                
+            }
+            return newframe;
+        }
+
+    }
+    #endregion
+
+    #region ClientCall
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Please find test values in Test project solution.
+            BowlingBallAlgo newGame = new BowlingBallAlgo();
+            int result = newGame.GetTotalScore(new List<int> { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 });
+            Console.WriteLine("Total Score: " + result);
+            Console.ReadKey();
+        }
+    }
+    #endregion
+}
---
+++ TestScore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BowlingScoreTest
+{
+    [TestClass]
+    public class TestScore
+    {
+
+        // No test cases written for input validations. So only one method is exposed in the service interface to calculate score.
+        // Assumptions that its 10 frames with 10th frame has 3 chance for the user to roll a ball.
+
+        [TestMethod]
+        public void Test_CalTotalScore_ValidInput_Expect_187()
+        {
+            BowllingBallTavisca.IBallingScore throwBalls = new BowllingBallTavisca.BowlingBallAlgo();
+            var result = throwBalls.GetTotalScore(new System.Collections.Generic.List<int> { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 });
+            Assert.IsTrue(result == 187);
+        }
+
+        [TestMethod]
+        public void Test_CalTotalScore_FirstStrikeFrame_Expect_168()
+        {
+            BowllingBallTavisca.IBallingScore throwBalls = new BowllingBallTavisca.BowlingBallAlgo();
+            var result = throwBalls.GetTotalScore(new System.Collections.Generic.List<int> { 10, 7, 3, 7, 2, 9, 1, 10, 10, 10, 2, 3, 6, 4, 7, 3, 3 });
+            Assert.IsTrue(result == 168);
+        }
+        [TestMethod]
+        public void Test_CalTotalScore_FirstSpareFrame_Expect_160()
+        {
+            BowllingBallTavisca.IBallingScore throwBalls = new BowllingBallTavisca.BowlingBallAlgo();
+            var result = throwBalls.GetTotalScore(new System.Collections.Generic.List<int> { 3,7, 7, 3, 7, 2, 9, 1, 10, 10, 10, 2, 3, 6, 4, 6, 3, 4 });
+            Assert.IsTrue(result == 160);
+        }
+        [TestMethod]
+        public void Test_CalTotalScore_AllStrike_Expect_PerfectScore()
+        {
+            BowllingBallTavisca.IBallingScore throwBalls = new BowllingBallTavisca.BowlingBallAlgo();
+            var result = throwBalls.GetTotalScore(new System.Collections.Generic.List<int> { 10,10,10,10,10,10,10,10,10,10, 10, 10});
+            Assert.IsTrue(result == 300);
+        }
+
+        [TestMethod]
+        public void Test_CalTotalScore_AllFalse_Expect_ZeroScore()
+        {
+            BowllingBallTavisca.IBallingScore throwBalls = new BowllingBallTavisca.BowlingBallAlgo();
+            var result = throwBalls.GetTotalScore(new System.Collections.Generic.List<int> { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 });
+            Assert.IsTrue(result == 0);
+        }
+
+        [TestMethod]
+        public void Test_CalTotalScore_NoStrikeNoSpare_Expect_67()
+        {
+            BowllingBallTavisca.IBallingScore throwBalls = new BowllingBallTavisca.BowlingBallAlgo();
+            var result = throwBalls.GetTotalScore(new System.Collections.Generic.List<int> { 1,3,2,6,5,3,8,1,5,4,0,0,2,6,1,3,8,0, 4,5,1});
+            Assert.IsTrue(result == 67);
+        }
+    }
+}
